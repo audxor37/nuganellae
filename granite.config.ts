8566e5d940e8f5bae6cf652a195dbd89c685555b
홍명택 <audxor240@naver.com>
2026-09-09 13:32:02 +0900

    Enable AppsInToss ads with updated banner group ID

--- granite.config.ts
+++ granite.config.ts
@@ -3,10 +3,10 @@ import { defineConfig } from '@apps-in-toss/web-framework/config';
 export default defineConfig({
   appName: 'nuganellae',
   brand: {
-    displayName: '누가낼래',
+    displayName: '누가낼래?',
     primaryColor: '#3182F6',
     // Replace this with the icon URL copied from AppsInToss Console app info.
-    icon: 'https://static.toss.im/appsintoss/59837/7dbf9e96-2d88-4747-a6b5-928a1a698f57.png',
+    icon: 'https://static.toss.im/appsintoss/59837/403e457f-5d60-4f8b-a6cc-4550c4c53a6e.png',
   },
   web: {
     host: '10.250.99.106',
